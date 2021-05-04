c2845cc386626f157d4f6f3249d0a851da112794
Kazuki Taninaka <onelive555@gmail.com>
2021-05-04 11:47:37 +0900

    fix typo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="bg-purple-400 bg-opacity-50">
       <div className="p-5 font-mono">
-        Kazuki Taninaka &copy;2021 All right reserved.
+        Kazuki Taninaka &copy;2021 All rights reserved.
       </div>
     </div>
   );
