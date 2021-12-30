7fa9f6f8025da13e4b2283c654b9061195b53013
Kazuki Taninaka <onelive555@gmail.com>
2021-12-30 10:46:30 +0900

    add image in preview (size is to fix)

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,5 +1,6 @@
 import { Dispatch, FC, SetStateAction } from "react";
 import ModifyFontSize from "./ModifyFontSize";
+import lockscreen from "../images/lockscreen.png";
 
 type Props = {
   setFontSize: Dispatch<SetStateAction<number>>;
@@ -16,7 +17,12 @@ const Preview: FC<Props> = ({ setFontSize, fontSize, img }) => {
         </div>
         <ModifyFontSize setFontSize={setFontSize} fontSize={fontSize} />
       </div>
-      {img && <img alt="icon" src={img} />}
+      <div className="relative">
+        {img && <img alt="icon" src={img} className="z-10" />}
+        <div className="absolute top-0 left-0 z-0">
+          <img src={lockscreen} alt="lockscreen" />
+        </div>
+      </div>
     </>
   );
 };
